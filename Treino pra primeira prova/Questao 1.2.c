2b92a71e2444a9fb8821651b763ce0de392093ca
ZaionLuz <Zaion0301@gmail.com>
2025-04-09 05:47:33 -0300

    Ajeitei

--- Treino pra primeira prova/Questao 1.2.c
+++ Treino pra primeira prova/Questao 1.2.c
@@ -16,10 +16,27 @@ int main(void)
     // Interpreta o retorno e mostra o resultado
     if (retorno == 0)
     {
-        printf("O numero: %d nao e primo", n);
+        printf("O numero: %d nao e primo\n", n);
     }
     else
     {
-        printf("O numero: %d e primo", n);
+        printf("O numero: %d e primo\n", n);
     }
+
+    return 0;
+}
+
+// Função que verifica se um número é primo
+int primo(int n)
+{
+    if (n <= 1)
+        return 0; // Números menores ou iguais a 1 não são primos
+
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+            return 0; // Se for divisível por algum número além de 1 e ele mesmo, não é primo
+    }
+
+    return 1; // Se passou por todos os testes, é primo
 }
